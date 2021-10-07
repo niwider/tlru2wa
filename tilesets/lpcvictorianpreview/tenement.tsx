4c462eb518850692bf9b642970a7c97c3899b0cb
niwider <n.wuchenauer@gmail.com>
2021-10-07 17:42:10 +0200

    + anas maps

---
+++ tilesets/lpcvictorianpreview/tenement.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="vten" tilewidth="32" tileheight="32" tilecount="3072" columns="32">
+ <image source="victorian-tenement.png" width="1024" height="3072"/>
+</tileset>
